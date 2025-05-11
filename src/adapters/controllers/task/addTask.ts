b8f73e78372df26f670450020cb9c42e594cada2
Liliana Silva <lilianaods@outlook.com>
2025-05-11 15:32:46 -0400

    refactor: implement Controller interface in AddTaskController

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -1,4 +1,5 @@
 import validator from "validator";
+import { Controller } from "../../interfaces/controller";
 import { HttpRequest, HttpResponse } from "../../interfaces/http";
 import { InvalidParamError } from "../../presentations/api/errors/invalid-param-error";
 import { MissingParamError } from "../../presentations/api/errors/missing-param-error";
@@ -6,28 +7,26 @@ import {
   badRequest,
   created,
 } from "../../presentations/api/httpResponses/httpResponses";
-import { Request, Response } from "express";
-
-export class AddTaskController {
-  async handle(req: Request, res: Response) {
+export class AddTaskController implements Controller {
+  handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ["title", "description", "date"];
 
     for (const field of requiredFields) {
-      if (!req.body[field]) {
-        return badRequest(new MissingParamError(field));
+      if (!httpRequest.body[field]) {
+        return Promise.resolve(badRequest(new MissingParamError(field)));
       }
     }
-    const { title, description, date } = req.body;
+    const { title, description, date } = httpRequest.body;
 
     const isValid = validator.isDate(date, {
       format: "DD-MM-YYYY",
     });
 
     if (!isValid) {
-      return badRequest(new InvalidParamError("date"));
+      return Promise.resolve(badRequest(new InvalidParamError("date")));
     }
 
     const task = { title, description, date };
-    res.json(created(task));
+    return Promise.resolve(created(task));
   }
 }
